7f465a0cba508df5652d5224a43a099d5cc6638f
Cassandra McCarthy <cassie@7596ff.com>
2017-01-11 13:54:53 -0500

    restructure patch, simplify logic fix #10

---
+++ commands/patch/hero.js
@@ -0,0 +1,18 @@
+const patch_list = require('../../json/patch.json')
+const short_heroes = require('../../json/short_heroes.json')
+
+const patch_hero_embed = require('./patch_hero_embed')
+
+module.exports = (message, client, helper, hero) => {
+    helper.log(message, `patch: hero name (${hero})`)
+    for (hero_list in patch_list.data) {
+        if (short_heroes[hero] in patch_list.data[hero_list]['heroes']) {
+            client.createMessage(message.channel.id, {
+                "embed": patch_hero_embed(short_heroes[hero], hero_list, helper.prefix)
+            }).then(new_message => {
+                helper.log(message, "  sent latest patch message")
+            }).catch(err => helper.log(message, err))
+            return
+        }
+    }
+}

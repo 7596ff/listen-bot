7633c193106b0070d0b0ae4a1edd4f2994b825da
Cassandra McCarthy <cassie@7596ff.com>
2017-01-14 04:35:56 -0500

    o

--- commands/eval.js
+++ commands/eval.js
@@ -1,4 +1,4 @@
-const clean  = (text) => {
+const clean = (text) => {
   if (typeof (text) === 'string') {
     return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
   } else {

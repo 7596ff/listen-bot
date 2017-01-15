41e62d62d7f0472fea675ea2707b8329ee11c47e
Cassandra McCarthy <cassie@7596ff.com>
2017-01-15 15:09:17 -0500

    better info, also test update command

--- commands/info.js
+++ commands/info.js
@@ -4,10 +4,14 @@ module.exports = (message, client, helper) => {
       {
         "name": "GitHub",
         "value": "https://github.com/bippum/listen-bot"
+      },
+      {
+        "name": "Invite to your server", 
+        "value": "https://discordapp.com/oauth2/authorize?permissions=19456&scope=bot&client_id=240209888771309568"
       }
     ],
     "timestamp": new Date().toJSON(),
-    "description": "A Dota 2 related bot by <@102645408223731712>. Features include patch notes for 7.00+ heroes, more in the pipeline."
+    "description": "A Dota 2 related bot. Features include current talents and patch notes for 6.79+ heroes. Contact <@102645408223731712> for support and questions!"
   }}).then(new_message => {
     helper.log(message, 'sent info message')
   }).catch(err => helper.log(message, err))

eec8eea3a710e714038266e4b0e5cbb32d43f3e9
Cassandra McCarthy <cassie@7596ff.com>
2017-01-15 17:21:55 -0500

    fix the case where there were no disabled commands before
    considering suicide

--- commands/admin/disable.js
+++ commands/admin/disable.js
@@ -10,20 +10,26 @@ function write_obj(guilds_list, message, helper) {
 module.exports = (message, client, helper) => {
     if (message.content) {
         let to_disable = message.content.split(' ')
-        let _disabled = []
+        client.guilds_list[message.guild.id].disabled[message.channel.id] 
+            = client.guilds_list[message.guild.id].disabled[message.channel.id] 
+            ? client.guilds_list[message.guild.id].disabled[message.channel.id] 
+            : []
+        var _disabled = client.guilds_list[message.guild.id].disabled[message.channel.id]
+
         for (cmd of to_disable) {
-            if (require('../../consts').cmdlist.indexOf(cmd) != -1) {
-                client.guilds_list[message.guild.id].disabled[message.channel.id].push(cmd)
+            if (_disabled.indexOf(cmd) == -1) {
                 _disabled.push(cmd)
             }
         }
 
         if (_disabled.length > 0) {
+            client.guilds_list[message.guild.id].disabled[message.channel.id] = _disabled
             write_obj(client.guilds_list, message, helper)
             client.createMessage(message.channel.id, `:ok_hand: disabled ${_disabled.join(', ')}`)
         }
     } else {
-        if (client.guilds_list[message.guild.id].disabled[message.channel.id].length > 0) {
+        if (client.guilds_list[message.guild.id].disabled[message.channel.id] 
+                && client.guilds_list[message.guild.id].disabled[message.channel.id].length > 0) {
             let send = `List of disabled commands: \`${client.guilds_list[message.guild.id].disabled[message.channel.id].join('\`, \`')}\``
             client.createMessage(message.channel.id, send)
         } else {

2825badfe3448ac946a75b6f9734cc1b37233688
Cassandra McCarthy <cassie@7596ff.com>
2017-01-17 13:52:35 -0500

    cleaning

--- commands/patch/version_hero.js
+++ commands/patch/version_hero.js
@@ -4,7 +4,7 @@ const short_heroes = require('../../json/short_heroes.json')
 const patch_hero_embed = require('./patch_hero_embed')
 
 module.exports = (message, client, helper, version, hero) => {
-    helper.log(message, `patch: version (${version}) and hero name (${hero})`)
+    helper.log(message, `patch: version (${version}) and hero name (${short_heroes[hero]})`)
     if (patch_list.data[patch_list.schema.indexOf(version)]['heroes'][short_heroes[hero]]) {
         client.createMessage(message.channel.id, {
             "embed": patch_hero_embed(short_heroes[hero], patch_list.schema.indexOf(version), helper.prefix)
@@ -12,14 +12,13 @@ module.exports = (message, client, helper, version, hero) => {
             helper.log(message, "  sent patch message")
         }).catch(err => helper.log(message, err))
     } else {
-        helper.log(message, "  couldn't find that version, sending latest")
         for (hero_list in patch_list.data) {
             if (short_heroes[hero] in patch_list.data[hero_list]['heroes']) {
                 client.createMessage(message.channel.id, {
                     "content": "Can't find that version! Here's the latest: ",
                     "embed": patch_hero_embed(short_heroes[hero], hero_list, helper.prefix)
                 }).then(new_message => {
-                    helper.log(message, "  sent latest patch message")
+                    helper.log(message, "  can't find that version, sent latest patch message")
                 }).catch(err => helper.log(message, err))
                 return
             }

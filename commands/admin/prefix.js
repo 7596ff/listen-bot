53d9cab0ba79e3ac4d45716e9d7d919a9515b115
Cassandra McCarthy <cassie@7596ff.com>
2017-01-17 10:04:36 -0500

    fix #13

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -9,7 +9,7 @@ function write_obj(guilds_list, message, helper) {
 
 module.exports = (message, client, helper) => {
     if (message.content) {
-        client.guilds_list[message.channel.guild.id].prefix = message.content.split(' ')[0]
+        client.guilds_list[message.channel.guild.id].prefix = message.content.replace('"', '')
         write_obj(client.guilds_list, message, helper)
         client.createMessage(message.channel.id, `:ok_hand: prefix set to \`${client.guilds_list[message.channel.guild.id].prefix}\``)
         helper.log(message, 'changed guild prefix')

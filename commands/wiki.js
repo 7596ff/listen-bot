9333fa6e1eadd1f8b0492cd61ad71b3afdbbd7f9
Brendan McCarthy <brendan10211@gmail.com>
2017-01-17 10:41:44 -0500

    add wiki command

---
+++ commands/wiki.js
@@ -0,0 +1,13 @@
+const talents = require('../json/talents.json')
+const short_heroes = require('../json/short_heroes.json')
+
+module.exports = (message, client, helper) => {
+    let options = message.content.split(' ')
+    options.shift()
+    let hero = options.join(' ')
+    if (hero in short_heroes) {
+        let underscore = talents[short_heroes[hero]].format_name.split(' ').join('_')
+        client.createMessage(message.channel.id, 
+            `<http://dota2.gamepedia.com/${underscore}>`)
+    }
+}

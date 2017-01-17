6382134a8082f728c8930ac941f6d0f0912c9523
Cassandra McCarthy <cassie@7596ff.com>
2017-01-17 11:19:27 -0500

    add list command (and help)

---
+++ commands/list.js
@@ -0,0 +1,27 @@
+const patch_list = require('../json/patch.json')
+const short_heroes = require('../json/short_heroes.json')
+const talents = require('../json/talents.json')
+
+module.exports = (message, client, helper) => {
+    let hero = message.content.toLowerCase().split(' ')
+    hero.shift()
+    hero = hero.join(' ')
+
+    if (hero in short_heroes) {
+        let patches = []
+        let found = 0
+        let patch_seq_num = 0
+        while (found < 10) {
+            if (patch_list.data[patch_seq_num]) {
+                if (patch_list.data[patch_seq_num].heroes[short_heroes[hero]]) {
+                    patches.push(patch_list.data[patch_seq_num].version)
+                    found++
+                }
+            } else {
+                found = 10
+            }
+            patch_seq_num++
+        }
+        client.createMessage(message.channel.id, `${talents[short_heroes[hero]].format_name} has changed in ${patches.join(', ')}`)
+    }
+}
--- consts.js
+++ consts.js
@@ -8,5 +8,6 @@ module.exports.cmdlist = [
     "admin",
     "stats",
     "shardinfo",
-    "wiki"
+    "wiki",
+    "list"
 ]

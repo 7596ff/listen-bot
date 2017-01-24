d546f1eaef12a6eaaec16ac5e3b315eb52cfc25a
Cassandra McCarthy <cassie@7596ff.com>
2017-01-24 17:27:48 -0500

    fix "the" etc being capitalized, loop fixes
    stop looking if we found something

--- commands/ability.js
+++ commands/ability.js
@@ -5,8 +5,11 @@ const alike_keys = require('../json/alike_keys.json')
 
 function capitalize_first(key) {
     words = key.split(' ')
+    short_words = ['of', 'and', 'in', 'the', 'de']
     for (word in words) {
-        words[word] = words[word].substr(0, 1).toUpperCase() + words[word].substr(1, words[word].length)
+        if (short_words.indexOf(words[word]) == -1) {
+            words[word] = words[word].substr(0, 1).toUpperCase() + words[word].substr(1, words[word].length)
+        }
     }
     return words.join(' ')
 }
@@ -82,19 +85,19 @@ module.exports = (message, client, helper) => {
         if (hero in short_heroes) {
             let true_hero = short_heroes[hero]
             if (true_hero == "invoker" && key.length == 3) key = key.split('').sort().join('')
-            if (key in keys[true_hero]) {
+            if (i > 0 && key in keys[true_hero]) {
                 let ability = keys[true_hero][key]
                 create_message(message, client, helper, true_hero, ability, key)
                 i = 0
             }
 
-            if (capitalize_first(key) in abilities[true_hero]) {
+            if (i > 0 && capitalize_first(key) in abilities[true_hero]) {
                 create_message(message, client, helper, true_hero, capitalize_first(key), key)
                 i = 0
             }
         } else if (!hero) {
             if (key.length > 1) {
-                if (key in alike_keys) {
+                if (i > 0 && key in alike_keys) {
                     content = alike_keys[key].length > 1 ? `Did you mean: ${alike_keys[key].join(', ')}` : alike_keys[key][0]
 
                     client.createMessage(message.channel.id, content).then(new_message => {
@@ -103,21 +106,21 @@ module.exports = (message, client, helper) => {
                     i = 0
                 } else {
                     for (key_obj in keys) {
-                        if (keys[key_obj][key]) {
+                        if (i > 0 && keys[key_obj][key]) {
                             create_message(message, client, helper, key_obj, keys[key_obj][key], key)
                             i = 0
-                        } else if (keys[key_obj][key.split('').sort().join('')]) {
-                            create_message(message, client, helper, key_obj, 
+                        } else if (i > 0 && keys[key_obj][key.split('').sort().join('')]) {
+                            create_message(message, client, helper, key_obj,
                                 keys[key_obj][key.split('').sort().join('')], key)
                             i = 0
                         }
                     }
-                    
+
                     for (hero_obj in abilities) {
-                        if (abilities[hero_obj][capitalize_first(key)]) {
-                            create_message(message, client, helper, hero_obj, 
+                        if (i > 0 && abilities[hero_obj][capitalize_first(key)]) {
+                            create_message(message, client, helper, hero_obj,
                                 capitalize_first(key), capitalize_first(key))
-                            i = 0 
+                            i = 0
                         }
                     }
                 }

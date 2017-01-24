97833e1291448095e57dcbe19e9669b7bc4d3af3
Brendan McCarthy <brendan10211@gmail.com>
2017-01-24 13:53:34 -0500

    add lookup by ability name and collision detection

--- commands/ability.js
+++ commands/ability.js
@@ -1,6 +1,7 @@
 const short_heroes = require('../json/short_heroes.json')
 const keys = require('../json/keys.json')
 const abilities = require('../json/abilities.json')
+const alike_keys = require('../json/alike_keys.json')
 
 function capitalize_first(key) {
     words = key.split(' ')
@@ -74,21 +75,53 @@ function create_message(message, client, helper, true_hero, ability, key) {
 module.exports = (message, client, helper) => {
     let options = message.content.toLowerCase().split(' ')
     options.shift()
-    for (let i = 0; i < options.length; i++) {
-        let key = capitalize_first(options.slice(options.length - i, options.length).join(' '))
+    for (let i = options.length; i > 0; i--) {
+        let key = options.slice(options.length - i, options.length).join(' ')
         let hero = options.slice(0, options.length - i).join(' ')
 
         if (hero in short_heroes) {
             let true_hero = short_heroes[hero]
-            if (true_hero == "invoker" && key.length == 3) key = key.toLowerCase().split('').sort().join('').toUpperCase()
-            
+            if (true_hero == "invoker" && key.length == 3) key = key.split('').sort().join('')
             if (key in keys[true_hero]) {
                 let ability = keys[true_hero][key]
                 create_message(message, client, helper, true_hero, ability, key)
+                i = 0
             }
-            
-            if (key in abilities[true_hero]) {
-                create_message(message, client, helper, true_hero, key, key)
+
+            if (capitalize_first(key) in abilities[true_hero]) {
+                create_message(message, client, helper, true_hero, capitalize_first(key), key)
+                i = 0
+            }
+        } else if (!hero) {
+            if (key.length > 1) {
+                if (key in alike_keys) {
+                    client.createMessage(message.channel.id, `Did you mean: ${alike_keys[key].join(', ')}`).then(new_message => {
+                        helper.log(message, `sent redirect for ${key}`)
+                    })
+                    i = 0
+                } else {
+                    for (key_obj in keys) {
+                        if (keys[key_obj][key]) {
+                            console.log(keys[key_obj][key])
+                            create_message(message, client, helper, key_obj, keys[key_obj][key], key)
+                            i = 0
+                        } else if (keys[key_obj][key.split('').sort().join('')]) {
+                            console.log(keys[key_obj][key.split('').sort().join('')])
+                            create_message(message, client, helper, key_obj, 
+                                keys[key_obj][key.split('').sort().join('')], key)
+                            i = 0
+                        }
+                    }
+                    
+                    for (hero_obj in abilities) {
+                        if (abilities[hero_obj][capitalize_first(key)]) {
+                            console.log(capitalize_first(key))
+                            create_message(message, client, helper, hero_obj, 
+                                capitalize_first(key), capitalize_first(key))
+                            i = 0 
+                        }
+                    }
+                }
             }
         }
     }

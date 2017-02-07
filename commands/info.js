4fada3bbb30a847863b06d57325300de09f8a88e
Cassandra McCarthy <cassie@7596ff.com>
2017-02-07 09:39:46 -0500

    :^)

--- commands/info.js
+++ commands/info.js
@@ -15,7 +15,7 @@ module.exports = (message, client, helper) => {
             }
         ],
         "timestamp": new Date().toJSON(),
-        "description": "A Dota 2 related bot. Features include current talents and patch notes for 6.79+ heroes. Contact alex#2899 for support and questions!"
+        "description": "A Dota 2 related bot. Features include current talents and patch notes for 6.79+ heroes. Contact alexa#2899 for support and questions!"
     }}).then(() => {
         helper.log(message, "sent info message");
     }).catch(err => helper.handle(message, err));

18ccc5922d09d6e56da83e7afc512c47e219df49
bippum <brendan10211@gmail.com>
2017-02-09 11:33:10 -0500

    make info compact, make invite simpler

--- commands/info.js
+++ commands/info.js
@@ -1,23 +1,16 @@
 module.exports = (message, client, helper) => {
     let me = message.channel.guild.members.has("102645408223731712") ? "<@102645408223731712>" : "alexa#2899";
 
+    let desc = [
+        `A Dota 2 related bot. Features include current \ntalents and patch notes for 6.79+ heroes. \nContact ${me} for support and questions!\n`,
+        ":page_facing_up: [GitHub](https://github.com/bippum/listen-bot)",
+        ":link: [Invite Link](https://discordapp.com/oauth2/authorize?permissions=19456&scope=bot&client_id=240209888771309568)",
+        `:information_source: [Help Server](${require("../json/config.json").discord_invite})`
+    ];
+
     client.createMessage(message.channel.id, {"embed": {
-        "fields": [
-            {
-                "name": "GitHub",
-                "value": "https://github.com/bippum/listen-bot"
-            },
-            {
-                "name": "Invite to your server", 
-                "value": "https://discordapp.com/oauth2/authorize?permissions=19456&scope=bot&client_id=240209888771309568"
-            },
-            {
-                "name": "Join our help server",
-                "value": require("../json/config.json").discord_invite
-            }
-        ],
         "timestamp": new Date().toJSON(),
-        "description": `A Dota 2 related bot. Features include current talents and patch notes for 6.79+ heroes. Contact ${me} for support and questions!`
+        "description": desc.join("\n")
     }}).then(() => {
         helper.log(message, "sent info message");
     }).catch(err => helper.handle(message, err));
--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,3 @@
 module.exports = (message, client, helper) => {
-    require("./info")(message, client, helper);
+    message.channel.createMessage("https://discordapp.com/oauth2/authorize?permissions=19456&scope=bot&client_id=240209888771309568");
 };

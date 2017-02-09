bb0c3a075a41cd469da6b526b42d1440d3331957
Cassandra McCarthy <cassie@7596ff.com>
2017-02-09 11:02:17 -0500

    lint

--- commands/patch/version_hero.js
+++ commands/patch/version_hero.js
@@ -7,7 +7,7 @@ module.exports = (message, client, helper, version, hero) => {
     helper.log(message, `patch: version (${version}) and hero name (${short_heroes[hero]})`);
     if (patch_list.data[patch_list.schema.indexOf(version)]["heroes"][short_heroes[hero]]) {
         client.createMessage(message.channel.id, {
-            "embed": patch_hero_embed(short_heroes[hero], patch_list.schema.indexOf(version), helper.prefix)
+            "embed": patch_hero_embed(short_heroes[hero], patch_list.schema.indexOf(version))
         }).then(() => {
             helper.log(message, "  sent patch message");
         }).catch(err => helper.handle(message, err));
@@ -16,7 +16,7 @@ module.exports = (message, client, helper, version, hero) => {
             if (short_heroes[hero] in patch_list.data[hero_list]["heroes"]) {
                 client.createMessage(message.channel.id, {
                     "content": "Can't find that version! Here's the latest: ",
-                    "embed": patch_hero_embed(short_heroes[hero], hero_list, helper.prefix)
+                    "embed": patch_hero_embed(short_heroes[hero], hero_list)
                 }).then(() => {
                     helper.log(message, "  can't find that version, sent latest patch message");
                 }).catch(err => helper.handle(message, err));

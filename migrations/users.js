aad34db61685b5ec4fbafb2196f63c33bc9c00d9
Cassandra McCarthy <cassie@7596ff.com>
2017-02-13 11:13:41 -0500

    node.js

--- migrations/users.js
+++ migrations/users.js
@@ -12,6 +12,7 @@ module.exports = client => {
             "text": "SELECT * FROM public.users;"
         }).then(res => {
             for (row in res.rows) {
+                let newrow = JSON.parse(JSON.stringify(res.rows[row]));
                 client.pg.query({
                     "text": "SELECT * FROM public.users WHERE id = $1;",
                     "values": [res.rows[row].id]
@@ -19,9 +20,9 @@ module.exports = client => {
                     if (newres.rowCount == 0) {
                         client.pg.query({
                             "text": "INSERT INTO public.users (id, steamid, dotaid) VALUES ($1, $2, $3);",
-                            "values": [res.rows[row].id, res.rows[row].steamid, res.rows[row].dotaid]
+                            "values": [newrow.id, newrow.steamid, newrow.dotaid]
                         }).then(() => {
-                            util.log(`  inserted dota id ${res.rows[row].id}`);
+                            util.log(`  inserted dota id ${newrow.id}`);
                         }).catch(err => {
                             util.log("  something went wrong inserting a user");
                             util.log(err);
@@ -29,9 +30,9 @@ module.exports = client => {
                     } else {
                         client.pg.query({
                             "text": "UPDATE public.users SET id = $1, steamid = $2, dotaid = $3 WHERE id = $1;",
-                            "values": [res.rows[row].id, res.rows[row].steamid, res.rows[row].dotaid]
+                            "values": [newrow.id, newrow.steamid, newrow.dotaid]
                         }).then(() => {
-                            util.log(`  updated dota id ${newres.rows[0].dotaid} -> ${res.rows[row].id}`);
+                            util.log(`  updated dota id ${newres.rows[0].dotaid} -> ${newrow.id}`);
                         }).catch(err => {
                             util.log("  something went wrong updating a user");
                             util.log(err);

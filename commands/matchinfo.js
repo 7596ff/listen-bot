503af53d8bde59cf9f08fdcf4024888b0588b6db
bippum <brendan10211@gmail.com>
2017-02-13 22:33:08 -0500

    add matchinfo and lastmatch
    i could probably util the resolution of acc id but im so done with this
    today

---
+++ commands/matchinfo.js
@@ -0,0 +1,79 @@
+const match_embed = require("../util/match_embed");
+
+function check_if_registered(client, dota_id, hero_id) {
+    return new Promise((resolve) => {
+        client.pg.query({
+            "text": "SELECT * FROM public.users WHERE dotaid = $1",
+            "values": [dota_id]
+        }).then(res => {
+            if (res.rowCount != 0) {
+                resolve({
+                    "discord_id": res.rows[0].id,
+                    "hero_id": hero_id
+                });
+            } else {
+                resolve(undefined);
+            }
+        });
+    });
+}
+
+function send_message(message, client, helper, match_data, origin) {
+    let queries = [];
+
+    match_data.players.forEach(player => {
+        queries.push(check_if_registered(client, player.account_id, player.hero_id));
+    });
+
+    Promise.all(queries).then(results => {
+        let filtered = results.filter(result => result && message.channel.guild.members.find(member => member.id == result.discord_id));
+        message.channel.createMessage({
+            "embed": match_embed(match_data, filtered),
+            "content": message.header || ""
+        }).then(() => {
+            helper.log(message, `  sent match data from ${origin}`);
+        }).catch(err => {
+            helper.log(message, "  something went wrong posting a match");
+            helper.log(message, err);
+        });
+    }).catch(err => {
+        helper.log(message, "  something went wrong selecting from the database");
+        helper.log(message, err);
+    });
+}
+
+module.exports = (message, client, helper) => {
+    let match_id = message.content.split(" ")[1];
+    if (!match_id) return;
+
+    if (match_id.match("dotabuff") || match_id.match("opendota")) {
+        let url = match_id.split("/");
+        match_id = url[url.length - 1];
+    }
+
+    if (isNaN(match_id)) {
+        message.channel.createMessage("I couldn't find a match ID in your message!");
+        return;
+    }
+
+    message.channel.sendTyping().then(() => {
+        client.redis.get(`matchinfo:${match_id}`, (err, reply) => {
+            if (err) helper.log(message, err);
+            if (reply) {
+                send_message(message, client, helper, JSON.parse(reply), "redis");
+            } else {
+                client.mika.getMatch(match_id).then(match_data => {
+                    send_message(message, client, helper, match_data, "api");
+                    client.redis.set(`matchinfo:${match_id}`, JSON.stringify(match_data), (err) => {
+                        if (err) helper.log(message, err);
+                        client.redis.expire(`matchinfo:${match_id}`, 604800);
+                    });
+                }).catch(err => {
+                    message.channel.createMessage("Something went wrong.");
+                    helper.log(message, "  something went wrong with mika");
+                    helper.log(message, err);
+                });
+            }
+        });
+    });
+};
---
+++ util/match_embed.js
@@ -0,0 +1,115 @@
+const pretty_ms = require("pretty-ms");
+const pad = require("pad");
+
+const od_heroes = require("../json/od_heroes.json");
+
+const game_modes = [
+    "Unknown",
+    "All Pick",
+    "Captain's Mode",
+    "Random Draft",
+    "Single Draft",
+    "All Random",
+    "Intro", // ???
+    "Diretide",
+    "Reverse Captain's Mode",
+    "Greevling",
+    "Tutorial", // k
+    "Mid Only",
+    "Least Played",
+    "Limited Heroes",
+    "Compendium Ranked",
+    "Custom", 
+    "Captian's Draft",
+    "Balanced Draft",
+    "Ability Draft",
+    "Event",
+    "ARDM", 
+    "1v1 Mid", 
+    "All Pick" // ranked all pick
+];
+
+const lobby_types = [
+    "Normal",
+    "Practice",
+    "Tournament",
+    "Tutorial",
+    "Co-op Bots",
+    "Team Ranked", 
+    "Solo Ranked",
+    "Party Ranked",
+    "1v1 Mid",
+    "Battle Cup"
+];
+
+const skills = [
+    "Unknown",
+    "Normal",
+    "High",
+    "Very High"
+];
+
+module.exports = (match_data, mentions) => {
+    let table = [["Name", "Hero", "K/D/A", "LH/D", "HD", "HH", "GPM", "XPM"]];
+    let ftable = [];
+    let highest = new Array(9).fill(0);
+
+    match_data.players.forEach(player => {
+        let row = [`${(player.personaname || "Unknown").slice(0, 16)}  `,
+            `${od_heroes.find(hero => hero.id == player.hero_id).localized_name.replace("Keeper of the Light", "KOTL")}  `,
+            `${player.kills}/${player.deaths}/${player.assists}`,
+            `${player.last_hits}/${player.denies}`,
+            player.hero_damage.toString(),
+            player.hero_healing.toString(),
+            player.xp_per_min.toString(),
+            player.gold_per_min.toString()
+        ];
+
+        for (let val in row) {
+            if (highest[val] < row[val].length) {
+                highest[val] = row[val].length;
+            }
+        }
+
+        table.push(row);
+    });
+
+    table.forEach(row => {
+        for (let item in row) {
+            row[item] = pad(row[item], highest[item]);
+        }
+        ftable.push(`\`${row.join(" ")}\``);
+    });
+
+    ftable.splice(1, 0, "Radiant");
+    ftable.splice(7, 0, "\nDire");
+
+    let victory = match_data.radiant_win ? "Radiant Victory!" : "Dire Victory!";
+    let ptime = pretty_ms(match_data.duration * 1000);
+    let gametype = `${lobby_types[match_data.lobby_type]} / ${game_modes[match_data.game_mode]}`;
+    let skill = skills[match_data.skill];
+    let mention_str = mentions.length > 0 
+        ? `\n\nMembers from this server: ${mentions.map(mention => `<@${mention.discord_id}> (${od_heroes.find(hero => hero.id == mention.hero_id).localized_name})`).join(", ")}` 
+        : "";
+
+    return {
+        "title": `${victory} ${match_data.radiant_score} - ${match_data.dire_score}, ${ptime}`,
+        "fields": [{
+            "name": "Lobby Type / Gamemode",
+            "value": gametype,
+            "inline": true
+        }, {
+            "name": `Skill: ${skill}`,
+            "value": `Match ID: ${match_data.match_id}`,
+            "inline": true
+        }, {
+            "name": "Links",
+            "value": `[DB](https://www.dotabuff.com/matches/${match_data.match_id}) / [OD](https://www.opendota.com/matches/${match_data.match_id})`,
+            "inline": true
+        }, {
+            "name": "Stats",
+            "value": `${ftable.join("\n")}${mention_str}`,
+            "inline": false
+        }]
+    };
+};

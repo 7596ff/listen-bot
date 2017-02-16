9dcdc252036a152ba429f5e89d5cfdfc63df431a
bippum <brendan10211@gmail.com>
2017-02-15 19:41:41 -0500

    unregister

---
+++ commands/unregister.js
@@ -0,0 +1,23 @@
+module.exports = (message, client, helper) => {
+    client.pg.query({
+        "text": "SELECT * FROM public.users WHERE id = $1;",
+        "values": [message.author.id]
+    }).then(res => {
+        console.log(res.rows[0])
+        if (res.rowCount > 0) {
+            client.pg.query({
+                "text": "DELETE FROM public.users WHERE id = $1;",
+                "values": [message.author.id]
+            }).then(() => {
+                message.channel.createMessage(":ok_hand: Deleted. Sorry to see you go...");
+                helper.log(message, "deleted a user");
+            });
+        } else {
+            message.channel.createMessage("You don't have a registration with me.");
+        }
+    }).catch(err => {
+        message.channel.createMessage("Couldn't delete. Contact alexa#2899");
+        helper.log(message, "something went wrong deleting from users");
+        helper.log(err);
+    })
+};

9bf3726225f889e76f3cedd7a071635cb7ffe9b0
bippum <brendan10211@gmail.com>
2017-02-16 21:24:33 -0500

    something i did here came out with lastmatch with

--- commands/lastmatch.js
+++ commands/lastmatch.js
@@ -1,105 +1,78 @@
 const matchinfo = require("./matchinfo");
-const resolve_user = require("../util/resolve_user");
+const resolve_dota_id = require("../util/resolve_dota_id");
 
 function send_message(message, client, helper, acc_id) {
-    helper.log(message, `lastmatch: ${acc_id}`);
 
-    message.channel.sendTyping().then(() => {
-        client.redis.get(`lastmatch:${acc_id}`, (err, res) => {
-            if (err) helper.log(message, err);
-            if (res) {
-                message.content = `matchinfo ${res}`;
-                matchinfo(message, client, helper);
-            } else {
-                client.mika.getPlayerMatches(acc_id, {
-                    "limit": "1"
-                }).then(match => {
-                    let match_id = match[0].match_id;
-                    message.content = `matchinfo ${match_id}`;
-                    matchinfo(message, client, helper);
-                    client.redis.set(`lastmatch:${acc_id}`, match_id, (err) => {
-                        if (err) helper.log(err);
-                        client.redis.expire(`lastmatch:${acc_id}`, 1800);
-                    });
-                });
-            }
-        });
-
-    });
 }
 
 module.exports = (message, client, helper) => {
-    if (message.mentions.length > 0) {
-        resolve_user(client, message.mentions[0].id).then(acc_id => {
-            send_message(message, client, helper, acc_id);
-        }).catch(err => {
-            if (err == "nouser") {
-                message.channel.createMessage(`That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`);
-            } else {
-                message.channel.createMessage("Something went wrong selecting this user from the database.");
-                helper.log(message, err);
+    message.channel.sendTyping().then(() => {
+        let check = message.content.split(" ")[1];
+        if (check && check == "with") {
+            let queries = [resolve_dota_id(message, message.author.id)];
+            for (ping in message.mentions) {
+                queries.push(resolve_dota_id(message, message.mentions[ping].id));
             }
-        });
-    } else {
-        let options = message.content.split(" ");
-        options.shift();
-        let acc_id = options[0];
-        let name = options.join(" ");
-        let inguild = message.channel.guild.members.find(member => (member.nick || member.username) == name);
-        let inclient = client.users.find(user => user.username == name);
 
-        if (inguild) {
-            resolve_user(client, inguild.id).then(acc_id => {
-                send_message(message, client, helper, acc_id);
-            }).catch(err => {
-                if (err == "nouser") {
-                    message.channel.createMessage(`That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`);
-                } else {
-                    message.channel.createMessage("Something went wrong selecting this user from the database.");
+            Promise.all(queries).then(results => {
+                helper.log(message, `lastmatch: ${results.join(", ")}`);
+                client.mika.getPlayerMatches(results[0], {
+                    "limit": 1,
+                    "included_account_id": results.slice(1)
+                }).then(match => {
+                    if (match) {
+                        let match_id = match[0].match_id;
+                        message.content = `matchinfo ${match_id}`;
+                        matchinfo(message, client, helper);
+                    }
+                }).catch(err => {
                     helper.log(message, err);
+                    message.channel.createMessage("Something went wrong.");
+                });
+            }).catch(err => {
+                if (err.err) {
+                    message.channel.createMessage(err.text);
+                    helper.log(message, err.text);
+                    helper.log(message, err.err);
+                } else {
+                    message.channel.createMessage(err.text);
+                }
+            })
+        } else {
+            resolve_dota_id(message).then(acc_id => {
+                helper.log(message, `lastmatch: ${acc_id}`);
+
+                client.redis.get(`lastmatch:${acc_id}`, (err, res) => {
+                    if (err) helper.log(message, err);
+                    if (res) {
+                        message.content = `matchinfo ${res}`;
+                        matchinfo(message, client, helper);
+                    } else {
+                        client.mika.getPlayerMatches(acc_id, {
+                            "limit": "1"
+                        }).then(match => {
+                            if (match) {
+                                let match_id = match[0].match_id;
+                                message.content = `matchinfo ${match_id}`;
+                                matchinfo(message, client, helper);
+                                client.redis.set(`lastmatch:${acc_id}`, match_id, (err) => {
+                                    if (err) helper.log(err);
+                                    client.redis.expire(`lastmatch:${acc_id}`, 1800);
+                                });
+                            } else {
+                                message.channel.createMessage("Couldn't find this player on Opendota!");
+                            }
+                        });
+                    }
+                });
+            }).catch(err => {
+                console.log(err)
+                if (err.err) {
+                    message.channel.createMessage(err.text);
+                    helper.log(message, err.text);
+                    helper.log(message, err.err);
                 }
             });
-            return;
         }
-
-        if (inclient) {
-            resolve_user(client, inclient.id).then(acc_id => {
-                send_message(message, client, helper, acc_id);
-            }).catch(err => {
-                if (err == "nouser") {
-                    message.channel.createMessage(`That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`);
-                } else {
-                    message.channel.createMessage("Something went wrong selecting this user from the database.");
-                    helper.log(message, err);
-                }
-            });
-            return;
-        }
-
-        if (!acc_id) {
-            resolve_user(client, message.author.id).then(acc_id => {
-                send_message(message, client, helper, acc_id);
-            }).catch(err => {
-                if (err == "nouser") {
-                    message.channel.createMessage(`You have not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`);
-                } else {
-                    message.channel.createMessage("Something went wrong selecting this user from the database.");
-                    helper.log(message, err);
-                }
-            });
-            return;
-        }
-
-        if (acc_id.match("dotabuff") || acc_id.match("opendota")) {
-            let url = acc_id.split("/");
-            acc_id = url[url.length - 1];
-        }
-
-        if (isNaN(acc_id)) {
-            message.channel.createMessage("I couldn't find an account ID in your message!");
-            return;
-        }
-
-        send_message(message, client, helper, acc_id);
-    }
+    });
 };
---
+++ util/resolve_dota_id.js
@@ -0,0 +1,114 @@
+const resolve_user = require("./resolve_user");
+
+module.exports = (message, from_discord) => {
+    return new Promise((resolve, reject) => {
+        if (from_discord) {
+            let inclient = message._client.users.find(user => user.id = from_discord);
+            if (inclient) {
+                resolve_user(message._client, inclient.id).then(acc_id => {
+                    resolve(acc_id);
+                }).catch(err => {
+                    if (err == "nouser") {
+                        reject({
+                            "text": `${inclient.username} has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
+                        });
+                    } else {
+                        reject({
+                            "text": "Something went wrong selecting this user from the database.",
+                            "err": err
+                        });
+                    }
+                });
+            }
+        } else {
+            if (message.mentions.length > 0) {
+                resolve_user(message._client, message.mentions[0].id).then(acc_id => {
+                    resolve(acc_id);
+                }).catch(err => {
+                    if (err == "nouser") {
+                        reject({
+                            "text": `That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
+                        });
+                    } else {
+                        reject({
+                            "text": "Something went wrong selecting this user from the database."
+                        });
+                    }
+                });
+            } else {
+                let options = message.content.split(" ");
+                options.shift();
+                let acc_id = options[0];
+                let name = options.join(" ");
+                let inguild = message.channel.guild.members.find(member => (member.nick || member.username) == name);
+                let inclient = message._client.users.find(user => user.username == name);
+
+                if (inclient || inguild) {
+                    if (inguild) {
+                        resolve_user(message._client, inguild.id).then(acc_id => {
+                            resolve(acc_id);
+                        }).catch(err => {
+                            if (err == "nouser") {
+                                reject({
+                                    "text": `That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
+                                });
+                            } else {
+                                reject({
+                                    "text": "Something went wrong selecting this user from the database.",
+                                    "err": err
+                                });
+                            }
+                        });
+                    }
+
+                    if (inclient) {
+                        resolve_user(message._client, inclient.id).then(acc_id => {
+                            resolve(acc_id);
+                        }).catch(err => {
+                            if (err == "nouser") {
+                                reject({
+                                    "text": `That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
+                                });
+                            } else {
+                                reject({
+                                    "text": "Something went wrong selecting this user from the database.",
+                                    "err": err
+                                });
+                            }
+                        });
+                    }
+                } else {
+                    if (acc_id) {
+                        if (acc_id.match("dotabuff") || acc_id.match("opendota")) {
+                            let url = acc_id.split("/");
+                            acc_id = url[url.length - 1];
+                        }
+
+                        if (isNaN(acc_id)) {
+                            reject({
+                                "text": "I couldn't find an account ID in your message!"
+                            });
+                        }
+
+                        resolve(acc_id);
+                    } else {
+                        resolve_user(message._client, message.author.id).then(acc_id => {
+                            resolve(acc_id);
+                        }).catch(err => {
+                            if (err == "nouser") {
+                                reject({
+                                    "text": `You have not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
+                                });
+                            } else {
+                                reject({
+                                    "text": "Something went wrong selecting this user from the database.",
+                                    "err": err
+                                });
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    });
+};

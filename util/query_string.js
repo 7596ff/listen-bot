c795b211a65b3b222474c19b7baf08447c97502f
bippum <brendan10211@gmail.com>
2017-02-16 19:30:57 -0500

    update query_string

--- util/query_string.js
+++ util/query_string.js
@@ -1,8 +1,14 @@
 function queryString(options) {
-    var str = [];
-    for (var p in options) {
+    let str = [];
+    for (let p in options) {
         if (options.hasOwnProperty(p)) {
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(options[p]));
+            if (Array.isArray(options[p])) {
+                for (let index of options[p]) {
+                    str.push(`${encodeURIComponent(p)}=${encodeURIComponent(index)}`);
+                }
+            } else {
+                str.push(`${encodeURIComponent(p)}=${encodeURIComponent(options[p])}`);
+            }
         }
     }
     return `?${str.join("&")}`;

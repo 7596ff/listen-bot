9ce769750a94d5612cb2378241c92b2195353bf1
Cassandra McCarthy <cassie@7596ff.com>
2017-02-18 15:49:06 -0500

    migrate patch

--- commands/patch/hero.js
+++ commands/patch/hero.js
@@ -1,18 +1,20 @@
 const patch_list = require("../../json/patch.json");
-const short_heroes = require("../../json/short_heroes.json");
+const find_hero = require("../../util/find_hero");
 
 const patch_hero = require("../../embeds/patch_hero");
 
 module.exports = (message, client, helper, hero) => {
     helper.log(message, `patch: hero name (${hero})`);
-    for (let hero_list in patch_list.data) {
-        if (short_heroes[hero] in patch_list.data[hero_list]["heroes"]) {
-            message.channel.createMessage({
-                "embed": patch_hero(short_heroes[hero], hero_list)
-            }).then(() => {
-                helper.log(message, "  sent latest patch message");
-            }).catch(err => helper.handle(message, err));
-            return;
+    find_hero(hero).then(res => {
+        for (let hero_list in patch_list.data) {
+            if (res in patch_list.data[hero_list]["heroes"]) {
+                message.channel.createMessage({
+                    "embed": patch_hero(res, hero_list)
+                }).then(() => {
+                    helper.log(message, "  sent latest patch message");
+                }).catch(err => helper.handle(message, err));
+                return;
+            }
         }
-    }
+    })
 };

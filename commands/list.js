8cdef9e1c01b02f96442a3fb8729a52dbe89bb82
bippum <brendan10211@gmail.com>
2017-02-18 15:39:09 -0500

    migrate wiki and list

--- commands/list.js
+++ commands/list.js
@@ -1,19 +1,20 @@
 const patch_list = require("../json/patch.json");
-const short_heroes = require("../json/short_heroes.json");
-const talents = require("../json/talents.json");
+const od_heroes = require("../json/od_heroes.json");
+const find_hero = require("../util/find_hero");
 
 module.exports = (message, client, helper) => {
     let hero = message.content.toLowerCase().split(" ");
     hero.shift();
     hero = hero.join(" ");
 
-    if (hero in short_heroes) {
+    find_hero(hero).then(res => {
+        let format_name = od_heroes.find(od_hero => od_hero.name == `npc_dota_hero_${res}`).localized_name;
         let patches = [];
         let found = false;
         let patch_seq_num = 0;
         while (!found) {
             if (patch_list.data[patch_seq_num]) {
-                if (patch_list.data[patch_seq_num].heroes[short_heroes[hero]]) {
+                if (patch_list.data[patch_seq_num].heroes[res]) {
                     patches.push(patch_list.data[patch_seq_num].version);
                 }
             } else {
@@ -21,8 +22,10 @@ module.exports = (message, client, helper) => {
             }
             patch_seq_num++;
         }
-        message.channel.createMessage(`${talents[short_heroes[hero]].format_name} has changed in ${patches.join(", ")}`).then(() => {
-            helper.log(message, `listed hero (${short_heroes[hero]})`);
+        message.channel.createMessage(`${format_name} has changed in ${patches.join(", ")}`).then(() => {
+            helper.log(message, `listed hero (${res})`);
         }).catch(err => helper.handle(message, err));
-    }
+    }).catch(err => {
+        helper.log(message, `list: couldn't find hero ${hero}`);
+    });
 };

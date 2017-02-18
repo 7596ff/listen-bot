c324226b3416f80bac7257ba2ec89d33ed8522d9
bippum <brendan10211@gmail.com>
2017-02-18 03:32:34 -0500

    tweak info to have dbots link in it

--- commands/info.js
+++ commands/info.js
@@ -5,13 +5,14 @@ module.exports = (message, client, helper) => {
         let user_me = client.users.get(client.config.owner);
         let me = message.channel.guild.members.has(client.config.owner) ? `<@${client.config.owner}>` : `${user_me.username}#${user_me.discriminator}`;
 
-        let desc = `A Dota 2 related bot. Features include current talents and patch notes for 6.79+ heroes. Contact ${me} for support and questions!`;
+        let desc = `A Dota 2 related bot. Contact ${me} for support and questions!`;
         let links = [":page_facing_up: [GitHub](https://github.com/bippum/listen-bot)",
+            "<:botTag:230105988211015680> [Online Help](https://bots.discord.pw/bots/240209888771309568)",
             `:link: [Invite Link](${client.config.url_invite})`,
             `:information_source: [Help Server](${client.config.discord_invite})`
         ];
 
-        let gitlinks = err ? ["rip github"] : response.body.slice(0, 3).map(commit => `[\`${commit.sha.slice(0, 6)}\`](${commit.html_url}) - ${commit.commit.message.slice(0, 35)}${commit.commit.message.length > 35 ? "..." : ""}`);
+        let gitlinks = err ? ["rip github"] : response.body.slice(0, 4).map(commit => `[\`${commit.sha.slice(0, 7)}\`](${commit.html_url}) - ${commit.commit.message.slice(0, 40)}${commit.commit.message.length > 40 ? "..." : ""}`);
 
         message.channel.createMessage({
             "embed": {

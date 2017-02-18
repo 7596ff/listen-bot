0973233574604615e654214710cb43b50ed0d1f8
Cassandra McCarthy <cassie@7596ff.com>
2017-02-18 15:39:09 -0500

    migrate wiki and list

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,14 +1,16 @@
-const talents = require("../json/talents.json");
-const short_heroes = require("../json/short_heroes.json");
+const find_hero = require("../util/find_hero");
+const od_heroes = require("../json/od_heroes.json");
 
 module.exports = (message, client, helper) => {
     let options = message.content.split(" ");
     options.shift();
     let hero = options.join(" ");
-    if (hero in short_heroes) {
-        let underscore = talents[short_heroes[hero]].format_name.split(" ").join("_");
+    find_hero(hero).then(res => {
+        let underscore = od_heroes.find(od_hero => od_hero.name == `npc_dota_hero_${res}`).localized_name.split(" ").join("_");
         message.channel.createMessage(`<http://dota2.gamepedia.com/${underscore}>`).then(() => {
-            helper.log(message, `sent wiki link (${short_heroes[hero]})`);
+            helper.log(message, `sent wiki link (${res})`);
         }).catch(err => helper.handle(message, err));
-    }
+    }).catch(err => {
+        helper.log(message, `wiki: couldn't find hero ${hero}`);
+    });
 };

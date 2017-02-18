ea90b36f679e18793caad68e74431b49d9c73b78
Cassandra McCarthy <cassie@7596ff.com>
2017-02-18 18:13:54 -0500

    add hero cmd

---
+++ commands/hero.js
@@ -0,0 +1,25 @@
+const abilities = require("../json/abilities.json");
+const find_hero = require("../util/find_hero");
+const heroes = require("../json/heroes.json");
+const hero_embed = require("../embeds/hero");
+
+module.exports = (message, client, helper) => {
+    let alias = message.content.split(" ").slice(1).join(" ");
+    helper.log(message, `hero: ${alias}`);
+    find_hero(alias).then(res => {
+        let hero_obj = heroes.find(hero => hero.true_name == res);
+        hero_obj.abilities = [];
+        for (skill in abilities[res]) hero_obj.abilities.push(skill);
+            
+        message.channel.createMessage({
+            "embed": hero_embed(hero_obj)
+        }).then(() => {
+            helper.log(message, "sent hero embed")
+        }).catch(err => {
+            helper.handle(message, err);
+        })
+    }).catch(err => {
+        helper.log(message, `hero: couldn't find ${alias}`);
+        helper.log(message, err);
+    });
+};

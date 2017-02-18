d123db1671e93ecf31eed1d76311c7d6579f91aa
bippum <brendan10211@gmail.com>
2017-02-18 15:49:06 -0500

    migrate patch

--- commands/patch/version_hero.js
+++ commands/patch/version_hero.js
@@ -1,27 +1,29 @@
 const patch_list = require("../../json/patch.json");
-const short_heroes = require("../../json/short_heroes.json");
+const find_hero = require("../../util/find_hero");
 
 const patch_hero = require("../../embeds/patch_hero");
 
 module.exports = (message, client, helper, version, hero) => {
-    helper.log(message, `patch: version (${version}) and hero name (${hero})`);
-    if (patch_list.data[patch_list.schema.indexOf(version)]["heroes"][short_heroes[hero]]) {
-        message.channel.createMessage({
-            "embed": patch_hero(short_heroes[hero], patch_list.schema.indexOf(version))
-        }).then(() => {
-            helper.log(message, "  sent patch message");
-        }).catch(err => helper.handle(message, err));
-    } else {
-        for (let hero_list in patch_list.data) {
-            if (short_heroes[hero] in patch_list.data[hero_list]["heroes"]) {
-                message.channel.createMessage({
-                    "content": "Can't find that version! Here's the latest: ",
-                    "embed": patch_hero(short_heroes[hero], hero_list)
-                }).then(() => {
-                    helper.log(message, "  can't find that version, sent latest patch message");
-                }).catch(err => helper.handle(message, err));
-                return;
+    find_hero(hero).then(res => {
+        helper.log(message, `patch: version (${version}) and hero name (${res})`);
+        if (patch_list.data[patch_list.schema.indexOf(version)]["heroes"][res]) {
+            message.channel.createMessage({
+                "embed": patch_hero(res, patch_list.schema.indexOf(version))
+            }).then(() => {
+                helper.log(message, "  sent patch message");
+            }).catch(err => helper.handle(message, err));
+        } else {
+            for (let hero_list in patch_list.data) {
+                if (res in patch_list.data[hero_list]["heroes"]) {
+                    message.channel.createMessage({
+                        "content": "Can't find that version! Here's the latest: ",
+                        "embed": patch_hero(res, hero_list)
+                    }).then(() => {
+                        helper.log(message, "  can't find that version, sent latest patch message");
+                    }).catch(err => helper.handle(message, err));
+                    return;
+                }
             }
         }
-    }
+    })
 };

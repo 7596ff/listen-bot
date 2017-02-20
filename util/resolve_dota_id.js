7dc6b2a16cd64ab3c9d2779b08b4a8fa38537207
Cassandra McCarthy <cassie@7596ff.com>
2017-02-20 11:42:49 -0500

    don't search the whole client for users anymore

--- util/resolve_dota_id.js
+++ util/resolve_dota_id.js
@@ -3,14 +3,14 @@ const resolve_user = require("./resolve_user");
 module.exports = (message, from_discord) => {
     return new Promise((resolve, reject) => {
         if (from_discord) {
-            let inclient = message._client.users.find(user => user.id == from_discord);
-            if (inclient) {
-                resolve_user(message._client, inclient.id).then(acc_id => {
+            let inguild = message.channel.guild.members.find(member => member.id == from_discord);
+            if (inguild) {
+                resolve_user(message._client, inguild.id).then(acc_id => {
                     resolve(acc_id);
                 }).catch(err => {
                     if (err == "nouser") {
                         reject({
-                            "text": `${inclient.username} has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
+                            "text": `${inguild.username} has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
                         });
                     } else {
                         reject({
@@ -41,42 +41,22 @@ module.exports = (message, from_discord) => {
                 let acc_id = options[0];
                 let name = options.join(" ");
                 let inguild = message.channel.guild.members.find(member => (member.nick || member.username) == name);
-                let inclient = message._client.users.find(user => user.username == name);
 
-                if (inclient || inguild) {
-                    if (inguild) {
-                        resolve_user(message._client, inguild.id).then(acc_id => {
-                            resolve(acc_id);
-                        }).catch(err => {
-                            if (err == "nouser") {
-                                reject({
-                                    "text": `That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
-                                });
-                            } else {
-                                reject({
-                                    "text": "Something went wrong selecting this user from the database.",
-                                    "err": err
-                                });
-                            }
-                        });
-                    }
-
-                    if (inclient) {
-                        resolve_user(message._client, inclient.id).then(acc_id => {
-                            resolve(acc_id);
-                        }).catch(err => {
-                            if (err == "nouser") {
-                                reject({
-                                    "text": `That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
-                                });
-                            } else {
-                                reject({
-                                    "text": "Something went wrong selecting this user from the database.",
-                                    "err": err
-                                });
-                            }
-                        });
-                    }
+                if (inguild) {
+                    resolve_user(message._client, inguild.id).then(acc_id => {
+                        resolve(acc_id);
+                    }).catch(err => {
+                        if (err == "nouser") {
+                            reject({
+                                "text": `That user has not registered with me yet! Try \`${message.gcfg.prefix}help register\`.`
+                            });
+                        } else {
+                            reject({
+                                "text": "Something went wrong selecting this user from the database.",
+                                "err": err
+                            });
+                        }
+                    });
                 } else {
                     if (acc_id) {
                         if (acc_id.match("dotabuff") || acc_id.match("opendota")) {

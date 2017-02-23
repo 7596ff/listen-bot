0db56530b6f509033f03915bc15fbf4e1a56574b
bippum <brendan10211@gmail.com>
2017-02-23 09:11:11 -0500

    i need to go to bed

--- commands/matchinfo.js
+++ commands/matchinfo.js
@@ -1,4 +1,5 @@
 const check_if_registered = require("../util/check_if_registered");
+const match_embed = require("../embeds/match")
 
 function send_message(message, client, helper, match_data, origin) {
     let queries = [];

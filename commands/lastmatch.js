98c41de831ed7843f72a4bed449e307c2ba14eeb
alexa <brendan10211@gmail.com>
2017-02-27 21:45:58 -0500

    experimental natural language search engine

--- commands/lastmatch.js
+++ commands/lastmatch.js
@@ -2,110 +2,86 @@ const matchinfo = require("./matchinfo");
 const resolve_dota_id = require("../util/resolve_dota_id");
 const search_members = require("../util/search_members");
 const find_hero = require("../util/find_hero");
+const eat = require("../util/eat");
 const od_heroes = require("../json/od_heroes.json");
 
-module.exports = (message, client, helper) => {
-    message.channel.sendTyping().then(() => {
-        message.content = message.content.replace(/<@[0-9]+>/g, "");
-        let options = message.content.split(" ");
+async function lastmatch(message, client, helper) {
+    await message.channel.sendTyping();
+    
+    let response = await eat(message, {
+        "with": "member",
+        "of": "member",
+        "as": "string"
+    });
 
-        let has_with = options.includes("with");
-        let has_as = options.includes("as");
-        let has_of = options[1] == "of";
+    if (response.err) {
+        message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
+        helper.log(message, response.err);
+        return;
+    }
 
-        let i_w = options.indexOf("with");
-        let i_a = options.indexOf("as");
-        let i_o = 1;
+    let qids = [];
 
-        let queries = [];
-        let hero_id = false;
+    if (response.with) {
+        qids.push(message.author.id);
+        qids.push(...response.with);
+        qids.filter((el, i, arr) => arr.indexOf(el) === i);
+    }
 
-        if (has_of) {
-            let of_who = options.slice(i_o + 1, (has_as && i_a > i_o ? i_a : options.length));
-            let results = search_members(message.channel.guild.members, of_who);
-            queries.push(...results.map(result => resolve_dota_id(message, result)));
-            queries.push(...message.mentions.map(user => resolve_dota_id(message, user.id)));
-            
-            if (has_as) {
-                let as_who = options.slice(i_a + 1, (has_of && i_o > i_a ? i_o : options.length));
-                find_hero(as_who.join(" ")).then(found_hero => {
-                    if (found_hero.not_found) {
-                        hero_id = false;
-                    } else {
-                        hero_id = od_heroes.find(hero => hero.name == `npc_dota_hero_${found_hero}`).id;
-                    }
-                });
-            }
-        } else {
-            queries.push(resolve_dota_id(message, message.author.id));
+    if (response.of) {
+        qids = [];
+        qids.push(...response.of);
+        if (response.with) qids.push(...response.with);
+        qids.filter((el, i, arr) => arr.indexOf(el) === i);
+    }
 
-            if (has_with) {
-                let with_who = options.slice(i_w + 1, (has_as && i_a > i_w ? i_a : options.length));
-                let results = search_members(message.channel.guild.members, with_who);
-                queries.push(...results.map(result => resolve_dota_id(message, result)));
-                queries.push(...message.mentions.map(user => resolve_dota_id(message, user.id)));
-            }
+    if (!response.with && !response.of) qids.push(message.author.id);
 
-            if (has_as) {
-                let as_who = options.slice(i_a + 1, (has_with && i_w > i_a ? i_w : options.length));
-                find_hero(as_who.join(" ")).then(found_hero => {
-                    if (found_hero.not_found) {
-                        hero_id = false;
-                    } else {
-                        hero_id = od_heroes.find(hero => hero.name == `npc_dota_hero_${found_hero}`).id;
-                    }
-                });
-            }
-        }
+    let found_hero = response.as ? await find_hero(response.as) : false;
+    let hero_id = found_hero ? od_heroes.find(hero => hero.name == `npc_dota_hero_${found_hero}`).id : false;
 
-        if (queries.length < (has_with ? 2 : 1)) {
-            message.channel.createMessage("Strange, I couldn't find any server members in your query. Make sure you formatted your command correctly.").catch(err => helper.handle(message, err));
-            helper.log(message, "strange query");
-            helper.log(message, message.content);
-            return;
-        }
-
-        Promise.all(queries).then(results => {
-            results.forEach(result => {
-                if (result.length < 1) {
-                    message.channel.createMessage("This user's account is private.").catch(err => helper.handle(message, err));
-                    return;
-                }
-            });
-
-            let logmsg = `lastmatch: ${results.join(", ")}`;
-            if (hero_id) logmsg += ` as ${hero_id}`;
-            helper.log(message, logmsg);
-
-            let mika_opt = {
-                "limit": 1,
-                "included_account_id": results.slice(1)
-            };
-
-            if (hero_id) mika_opt.hero_id = hero_id;
-
-            client.mika.getPlayerMatches(results[0], mika_opt).then(match => {
-                if (match) {
-                    let match_id = match[0].match_id;
-                    message.content = `matchinfo ${match_id}`;
-                    matchinfo(message, client, helper);
-                }
-            }).catch(err => {
-                helper.log(message, err);
-                message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
-            });
-        }).catch(err => {
-            if (err.err) {
-                message.channel.createMessage(err.text || "Something went wrong.").catch(err => helper.handle(message, err));
-                helper.log(message, err.text);
-                helper.log(message, err.err);
-            } else if (err.text) {
-                message.channel.createMessage(err.text).catch(err => helper.handle(message, err));
-                helper.log(message, err.log);
-            } else {
-                message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
-                helper.log(message, err);
+    Promise.all(qids.map(id => resolve_dota_id(message, id))).then(results => {
+        results.forEach(result => {
+            if (result.length < 1) {
+                message.channel.createMessage("This user's account is private.").catch(err => helper.handle(message, err));
+                return;
             }
         });
+
+        let logmsg = `lastmatch: ${results.join(", ")}`;
+        if (hero_id) logmsg += ` as ${hero_id}`;
+        helper.log(message, logmsg);
+
+        let mika_opt = {
+            "limit": 1,
+            "included_account_id": results.slice(1)
+        };
+
+        if (hero_id) mika_opt.hero_id = hero_id;
+
+        client.mika.getPlayerMatches(results[0], mika_opt).then(match => {
+            if (match) {
+                let match_id = match[0].match_id;
+                message.content = `matchinfo ${match_id}`;
+                matchinfo(message, client, helper);
+            }
+        }).catch(err => {
+            helper.log(message, err);
+            message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
+        });
+    }).catch(err => {
+        if (err.err) {
+            message.channel.createMessage(err.text || "Something went wrong.").catch(err => helper.handle(message, err));
+            helper.log(message, err.text);
+            helper.log(message, err.err);
+        } else if (err.text) {
+            message.channel.createMessage(err.text).catch(err => helper.handle(message, err));
+            helper.log(message, err.log);
+        } else {
+            message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
+            helper.log(message, err);
+        }
     });
 };
+
+module.exports = lastmatch;
---
+++ util/eat.js
@@ -0,0 +1,38 @@
+const search_members = require("./search_members");
+
+function eat(message, options) {
+	return new Promise((resolve, reject) => {
+		let searches = Object.entries(options).map(entry => entry[0]);
+		let responses = {};
+
+		for (search in options) {
+			let type = options[search];
+			let split = message.content.split(" ");
+			let not_search = searches.slice(0);
+			not_search.splice(searches.indexOf(search), 1);
+
+			let first_found = split.includes(search) ? split.indexOf(search) : false;
+
+			let outer_bound = false;
+			not_search.forEach(term => {
+				if (split.indexOf(term) > split.indexOf(search)) outer_bound = split.indexOf(term);
+			});
+
+			if (first_found) {
+				let res = split.slice(first_found + 1, outer_bound || split.length);
+				switch(type) {
+					case "string": 
+						responses[search] = res.join(" ");
+						break;
+					case "member": 
+						responses[search] = search_members(message.channel.guild.members, res);
+						break;
+				}
+			}
+		}
+
+		resolve(responses);
+	});
+}
+
+module.exports = eat;

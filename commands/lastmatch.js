b91b7f5d27c1ad0fccccead75fda80cd2256a95a
Cassandra McCarthy <cassie@7596ff.com>
2017-03-02 19:58:34 -0500

    actual goodcode?

--- commands/lastmatch.js
+++ commands/lastmatch.js
@@ -37,58 +37,60 @@ async function lastmatch(message, client, helper) {
 
     if (!response.with && !response.of) qids.push(message.author.id);
 
-    let found_hero = false;
-    try {
-        found_hero = await find_hero(response.as);
+    try { 
+        let found_hero = await find_hero(response.as);
+        let hero_id = od_heroes.find(hero => hero.name == `npc_dota_hero_${found_hero}`).id;
+
+        Promise.all(qids.map(id => resolve_dota_id(message, id))).then(results => {
+            results.forEach(result => {
+                if (result.length < 1) {
+                    message.channel.createMessage("This user's account is private.").catch(err => helper.handle(message, err));
+                    return;
+                }
+            });
+
+            let logmsg = `lastmatch: ${results.join(", ")}`;
+            if (hero_id) logmsg += ` as ${hero_id}`;
+            helper.log(message, logmsg);
+
+            let mika_opt = {
+                "limit": 1,
+                "included_account_id": results.slice(1)
+            };
+
+            if (hero_id) mika_opt.hero_id = hero_id;
+
+            client.mika.getPlayerMatches(results[0], mika_opt).then(match => {
+                if (match) {
+                    let match_id = match[0].match_id;
+                    message.content = `matchinfo ${match_id}`;
+                    matchinfo(message, client, helper);
+                }
+            }).catch(err => {
+                helper.log(message, err);
+                message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
+            });
+        }).catch(err => {
+            if (err.err) {
+                message.channel.createMessage(err.text || "Something went wrong.").catch(err => helper.handle(message, err));
+                helper.log(message, err.text);
+                helper.log(message, err.err);
+            } else if (err.text) {
+                message.channel.createMessage(err.text).catch(err => helper.handle(message, err));
+                helper.log(message, err.log);
+            } else {
+                message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
+                helper.log(message, err);
+            }
+        });
     } catch (err) {
         if (err === false) {
             helper.log(message, `couldn't find hero ${response.as}`);
+            message.channel.createMessage("I couldn't find that hero.").catch(err => helper.handle(message, err));
+            return;
         }
     }
-    let hero_id = found_hero ? od_heroes.find(hero => hero.name == `npc_dota_hero_${found_hero}`).id : false;
 
-    Promise.all(qids.map(id => resolve_dota_id(message, id))).then(results => {
-        results.forEach(result => {
-            if (result.length < 1) {
-                message.channel.createMessage("This user's account is private.").catch(err => helper.handle(message, err));
-                return;
-            }
-        });
-
-        let logmsg = `lastmatch: ${results.join(", ")}`;
-        if (hero_id) logmsg += ` as ${hero_id}`;
-        helper.log(message, logmsg);
-
-        let mika_opt = {
-            "limit": 1,
-            "included_account_id": results.slice(1)
-        };
-
-        if (hero_id) mika_opt.hero_id = hero_id;
-
-        client.mika.getPlayerMatches(results[0], mika_opt).then(match => {
-            if (match) {
-                let match_id = match[0].match_id;
-                message.content = `matchinfo ${match_id}`;
-                matchinfo(message, client, helper);
-            }
-        }).catch(err => {
-            helper.log(message, err);
-            message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
-        });
-    }).catch(err => {
-        if (err.err) {
-            message.channel.createMessage(err.text || "Something went wrong.").catch(err => helper.handle(message, err));
-            helper.log(message, err.text);
-            helper.log(message, err.err);
-        } else if (err.text) {
-            message.channel.createMessage(err.text).catch(err => helper.handle(message, err));
-            helper.log(message, err.log);
-        } else {
-            message.channel.createMessage("Something went wrong.").catch(err => helper.handle(message, err));
-            helper.log(message, err);
-        }
-    });
 };
 
 module.exports = lastmatch;

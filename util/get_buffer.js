90947c2987ab7ee916ef60353df7db745ee017b5
Cassandra McCarthy <cassie@7596ff.com>
2017-03-04 00:25:55 -0500

    add divine courage

---
+++ util/get_buffer.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const http = require("http");
+
+module.exports = (url, dirname) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dirname.split("/").slice(0, -1).join("/"), (err, files) => {
+            if (files.includes(dirname.split("/").slice(-1)[0])) {
+                fs.readFile(dirname, (err, data) => {
+                    if (err) reject(err);
+                    resolve(data);
+                });
+            } else {
+                let file = fs.createWriteStream(dirname);
+                http.get(url, (response) => {
+                    response.pipe(file).on("finish", () => {
+                        fs.readFile(dirname, (err, data) => {
+                            if (err) reject(err);
+                            resolve(data);
+                        });
+                    });
+                });
+            }
+        });
+    });
+};

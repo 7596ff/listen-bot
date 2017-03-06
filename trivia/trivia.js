0d6c468d8800f7e304a8dea85dd1cb6494955da7
brendan10211 <brendan10211@gmail.com>
2017-03-06 16:58:22 -0500

    start work for trivia

---
+++ trivia/trivia.js
@@ -0,0 +1,12 @@
+class Trivia {
+    constructor(questions) {
+        this.questions = questions;
+        this.channels = [];
+    }
+
+    handle() {
+        ///kek
+    }
+}
+
+module.exports = Trivia;

2e77958cb18a614c3e50a81862023d0d8e2b9373
brendan10211 <brendan10211@gmail.com>
2017-03-07 09:39:45 -0500

    cleaner trivia config

--- commands/admin/trivia.js
+++ commands/admin/trivia.js
@@ -1,29 +1,36 @@
-module.exports = (message, client, helper) => {
-    if (message.channelMentions && message.channelMentions.length == 1) {
-        client.pg.query({
-            "text": "UPDATE public.guilds SET trivia = $1 WHERE id = $2",
-            "values": [message.channelMentions[0], message.channel.guild.id]
-        }).then(() => {
-            message.channel.createMessage(`:ok_hand: trivia enabled in <#${message.channelMentions[0]}>`).then(() => {
-                helper.log(message, `changed trivia channel to ${message.channelMentions[0]}`);
-            }).catch(err => helper.handle(message, err));
-        }).catch(err => {
-            helper.log(message, "something went wrong with updating trivia channel");
-            helper.log(message, err);
-        });
-    } else {
-        client.pg.query({
-            "text": "UPDATE public.guilds SET trivia = $1 WHERE id = $2",
-            "values": [0, message.channel.guild.id]
-        }).then(() => {
-            message.channel.createMessage(":ok_hand: trivia disabled").then(() => {
-                helper.log(message, "removed trivia channel");
-            }).catch(err => helper.handle(message, err));
-        }).catch(err => {
-            helper.log(message, "something went wrong with updating trivia channel");
-            helper.log(message, err);
-        });
+const help = require("../help");
 
-        if (client.trivia.channels.includes(message.gcfg.trivia)) client.trivia.channels.splice(client.trivia.channels.indexOf(message.gcfg.trivia), 1);
+function edit_trivia(pg, channel, message, helper) {
+    pg.query({
+        "text": "UPDATE public.guilds SET trivia = $1 WHERE id = $2",
+        "values": [channel || 0, message.channel.guild.id]
+    }).then(() => {
+        let msg = channel ? `enabled in channel <#${channel}>.` : "disabled.";
+        message.channel.createMessage(`:ok_hand: Trivia ${msg}`).then(() => {
+            helper.log(message, `changed trivia channel to ${channel || "none"}`);
+        }).catch(err => helper.handle(message, err));
+    }).catch(err => {
+        helper.log(message, "something went wrong with updating trivia channel");
+        helper.log(message, err);
+    });
+}
+
+module.exports = (message, client, helper) => {
+    const split_content = message.content.split(" ");
+    switch(split_content[0]) {
+    case "channel":
+        if (message.channelMentions.length > 0) {
+            edit_trivia(client.pg, message.channelMentions[0], message, helper);
+        } else if (split_content.slice(1).join(" ").trim() == "here") {
+            edit_trivia(client.pg, message.channel.id, message, helper);
+        } else if (split_content.slice(1).join(" ").trim() == "none") {
+            if (client.trivia.channels.includes(message.gcfg.trivia)) client.trivia.channels.splice(client.trivia.channels.indexOf(message.gcfg.trivia), 1);
+            edit_trivia(client.pg, null, message, helper);
+        }
+        break;
+    default:
+        message.content = "help admin trivia";
+        help(message, client, helper);
+        break;
     }
 };

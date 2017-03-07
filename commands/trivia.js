e20f3ec7a8c51382ce76b7d0e2ee2a2fb456ec26
brendan10211 <brendan10211@gmail.com>
2017-03-07 13:33:51 -0500

    view leaderboard

--- commands/trivia.js
+++ commands/trivia.js
@@ -1,8 +1,7 @@
 module.exports = (message, client, helper) => {
-    const command = message.content.split(" ").slice(1)[0];
-
-    switch (command) {
-    case "start":
+    const split_content = message.content.split(" ").slice(1)
+    const command = split_content[0];
+    if (command == "start") {
         if (message.gcfg.trivia == message.channel.id) {
             client.trivia.init(client, message.channel.id);
         } else if (message.gcfg.trivia != 0 && message.gcfg.trivia != message.channel.id) {
@@ -10,11 +9,32 @@ module.exports = (message, client, helper) => {
         } else {
             message.channel.createMessage(`This server does not have a desginated trivia channel! Try \`${message.gcfg.prefix}help admin trivia\`.`);
         }
-        break;
-    case "stop":
+    }
+
+    if (command == "stop") {
         // if (!message.member.permission.has("manageMessages")) break;
         if (client.trivia.channels.includes(message.gcfg.trivia)) client.trivia.channels.splice(client.trivia.channels.indexOf(message.gcfg.trivia), 1);
         message.channel.createMessage(":ok_hand: Trivia stopped.").catch(err => helper.handle(message, err));
-        break;
+    }
+
+    if (["top", "leaderboard", "scores", "points"].includes(command)) {
+        const subcommand = split_content[1];
+
+        client.pg.query("SELECT * FROM scores ORDER BY score DESC;").then(res => {
+            let rows = res.rows;
+            let msg = [];
+            if (subcommand == "all") {
+                msg.push("Top 10 users throughout bot: ");
+            } else {
+                rows = rows.filter(row => message.channel.guild.members.get(row.id));
+                msg.push("Top 10 users in this server: ");
+            }
+            rows = rows.slice(0, 10);
+            msg.push(...rows.map(row => `${client.users.find(user => user.id == row.id).username}: ${row.score}`));
+            message.channel.createMessage(msg.join("\n")).catch(err => helper.handle(message, err));
+        }).catch(err => {
+            helper.log(message, "something went wrong selecting scores");
+            helper.log(message, err);
+        });
     }
 };

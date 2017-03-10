a9983f0b934d4c94c42f6bd4af3f45e5eef38076
brendan10211 <brendan10211@gmail.com>
2017-03-10 13:06:25 -0500

    i want to kill

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 module.exports = (message, client, helper) => {
-    message.channel.createMessage(rec).then(new_m => {
+    message.channel.createMessage(message.timestamp).then(new_m => {
         new_m.edit(`:ping_pong: Pong! Message send latency: \`${new_m.timestamp - message.timestamp} ms\``).catch(err => helper.handle(message, err));
     }).catch(err => helper.handle(message, err));
 };

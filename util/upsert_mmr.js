895d3af8107995eec47abc4b0d4bafb048f1ae7b
alexa <brendan10211@gmail.com>
2017-03-11 01:35:17 -0500

    add mmr stuffs

---
+++ util/upsert_mmr.js
@@ -0,0 +1,28 @@
+function upsert_mmr(pg, mika, row) {
+    return new Promise((resolve, reject) => {
+        if (row.sat && parseInt(row.sat) + 604800000 > Date.now()) {
+            resolve(row);
+        } else {
+            mika.getPlayer(row.dotaid).then(player => {
+                let now = Date.now();
+                pg.query({
+                    "text": "UPDATE public.users SET scr = $1, cr = $2, sat = $3 WHERE dotaid = $4;",
+                    "values": [player.solo_competitive_rank || 0, player.competitive_rank || 0, now, row.dotaid]
+                }).then(() => {
+                    row.scr = player.solo_competitive_rank;
+                    row.cr = player.competitive_rank;
+                    row.sat = now;
+                    resolve(row);
+                }).catch(err => {
+                    err._origin = "postgres";
+                    reject(err);
+                });
+            }).catch(err => {
+                err._origin = "mika";
+                reject(err);
+            });
+        }
+    });
+}
+
+module.exports = upsert_mmr;

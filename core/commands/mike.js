3680ef1c7d82053c6228a4be62342d41f3f81bdb
brendan10211 <brendan10211@gmail.com>
2017-04-23 19:22:45 -0400

    fix mike command

--- core/commands/mike.js
+++ core/commands/mike.js
@@ -1,6 +1,6 @@
 module.exports = (message, client, helper) => {
     let mike = client.core.json.ixmike;
-    message.channel.createMessage(mike[Math.floor(Math.random() * mike.length)]).catch(err => helper.handle(err)).then((msg) => {
+    message.channel.createMessage(mike[Math.floor(Math.random() * mike.length)]).catch(err => helper.handle(message, err)).then((msg) => {
         if (msg.channel.guild.id == "137589613312081920") msg.addReaction("ixmikeW:256896118380691466");
     });
 };

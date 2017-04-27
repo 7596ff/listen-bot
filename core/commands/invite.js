bf77b09867fa752f18529f3960b1d4a01876aea4
Zain Wania <zainthegreatcb@gmail.com>
2017-04-27 08:59:57 -0400

    Update invite link
    no more embed! hopefully...

--- core/commands/invite.js
+++ core/commands/invite.js
@@ -1,3 +1,3 @@
 module.exports = (message, client, helper) => {
-    message.channel.createMessage(`https://discordapp.com/oauth2/authorize?permissions=${client.config.permissions}&scope=bot&client_id=${client.user.id}`).catch(err => helper.handle(message, err));
+    message.channel.createMessage(`<https://discordapp.com/oauth2/authorize?permissions=${client.config.permissions}&scope=bot&client_id=${client.user.id}>`).catch(err => helper.handle(message, err));
 };

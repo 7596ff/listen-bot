ba8c4a3a5177825b99c0b7954104f9faaf3be33b
brendan10211 <brendan10211@gmail.com>
2017-05-15 13:13:40 -0400

    fix botstats

--- commands/meta/botstats.js
+++ commands/meta/botstats.js
@@ -2,7 +2,7 @@ const statsEmbed = require("../../templates/stats");
 
 async function exec(ctx) {
     try {
-        let embed = await ctx.client.core.templates.stats(ctx.client);
+        let embed = await statsEmbed(ctx.client);
         return ctx.embed(embed);
     } catch (err) {
         console.error(err);

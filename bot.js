3d14727fd3342384d4b4b5ceb38d03ca0e851459
Cassandra McCarthy <cassie@7596ff.com>
2017-05-18 23:34:23 -0400

    lets hope this does something

--- bot.js
+++ bot.js
@@ -356,9 +356,8 @@ async function publishMatches(data) {
     for (guildID in stacks) {
         let players = stacks[guildID];
         let gcfg = await cacheGcfg(guildID);
-        if (players.length < (gcfg.threshold || 5)) continue;
 
-        if (data.found.player.every((id) => players.includes(parseInt(id)))) {
+        if (players.length >= (gcfg.threshold || 5) && data.found.player.every((id) => players.includes(parseInt(id)))) {
             allChannels.push(allRows.find((row) => row.owner === guildID).channel);
         }
     }

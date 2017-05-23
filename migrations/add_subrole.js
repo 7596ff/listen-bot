6c1ca4353e5f483ccee2cfa4a31523aac1509db3
Cassandra McCarthy <cassie@7596ff.com>
2017-05-23 11:52:41 -0400

    this is some really bad code i think

--- migrations/add_subrole.js
+++ migrations/add_subrole.js
@@ -1,3 +1,3 @@
 module.exports = (pg) => {
-    return pg.query("alter table guilds add column subrole bigint default 0; create unique index subrole_index on subs (owner, type, value)");
+    return pg.query("alter table guilds add column subrole bigint default 0;");
 }

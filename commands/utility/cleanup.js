9f8790f649d717caf4b24cb58c70e18b74c723b4
Cassandra McCarthy <cassie@7596ff.com>
2017-09-19 20:34:21 -0400

    fix cleanup

--- commands/utility/cleanup.js
+++ commands/utility/cleanup.js
@@ -4,12 +4,14 @@ async function exec(ctx) {
             .filter((message) => message.author.id == ctx.client.user.id)
             .map((message) => message.id);
 
-        let promises = ids.map((id) => ctx.client.deleteMessage(ctx.channel.id, id));
-        await Promise.all(promises);
+        for (let id in ids) {
+            try { await ctx.client.deleteMessage(ctx.channel.id, id) } catch (err) {}
+        }
+
         return ctx.delete(5000, ":white_check_mark:");
     } catch (err) {
         ctx.error(err);
-        return ctx.failure(ctx.trings.get("bot_generic_error"));
+        return ctx.failure(ctx.strings.get("bot_generic_error"));
     }
 }
 

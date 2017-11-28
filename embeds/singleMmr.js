2ca2655f0a932be2c22f646f3a0f381fa35ab37d
brendan10211 <brendan10211@gmail.com>
2017-11-27 20:55:40 -0500

    badge update

--- embeds/singleMmr.js
+++ embeds/singleMmr.js
@@ -1,16 +1,86 @@
-function singleMmr(obj) {
+const snekfetch = require("snekfetch");
+const tnhConfig = require("../config.json").thinknohands;
+
+const ranks = {
+    "00": "Uncalibrated",
+
+    "10": "Herald",
+    "11": "Herald [1]",
+    "12": "Herald [2]",
+    "13": "Herald [3]",
+    "14": "Herald [4]",
+    "15": "Herald [5]",
+    
+    "20": "Guardian",
+    "21": "Guardian [1]",
+    "22": "Guardian [2]",
+    "23": "Guardian [3]",
+    "24": "Guardian [4]",
+    "25": "Guardian [5]",
+    
+    "30": "Crusader",
+    "31": "Crusader [1]",
+    "32": "Crusader [2]",
+    "33": "Crusader [3]",
+    "34": "Crusader [4]",
+    "35": "Crusader [5]",
+    
+    "40": "Archon",
+    "41": "Archon [1]",
+    "42": "Archon [2]",
+    "43": "Archon [3]",
+    "44": "Archon [4]",
+    "45": "Archon [5]",
+    
+    "50": "Legend",
+    "51": "Legend [1]",
+    "52": "Legend [2]",
+    "53": "Legend [3]",
+    "54": "Legend [4]",
+    "55": "Legend [5]",
+    
+    "60": "Ancient",
+    "61": "Ancient [1]",
+    "62": "Ancient [2]",
+    "63": "Ancient [3]",
+    "64": "Ancient [4]",
+    "65": "Ancient [5]",
+    
+    "70": "Divine",
+    "71": "Divine [1]",
+    "72": "Divine [2]",
+    "73": "Divine [3]",
+    "74": "Divine [4]",
+    "75": "Divine [5]",
+    "76": "Divine [5]"
+}
+
+async function singleMmr(data) {
+    let url = `${tnhConfig.url}/rank?key=${tnhConfig.key}&rank=${data.rank}`;
+    let image = await snekfetch.get(url);
+
     return {
-        "author": {
-            "icon_url": obj.member.avatarURL,
-            "name": obj.member.username
+        "file": {
+            "name": "rank.png",
+            "file": image.body
         },
-        "description": [
-            `**Solo MMR:** ${obj.scr || this.get("matches_match_unknown_player")}`,
-            `**Party MMR:** ${obj.cr || this.get("matches_match_unknown_player")}`
-        ].join("\n"),
-        "timestamp": new Date(obj.sat ? parseInt(obj.sat) : Date.now()),
-        "footer": {
-            "text": this.get("prommr_last_updated")
+        "embed": {
+            "author": {
+                "icon_url": data.member.avatarURL,
+                "name": `${data.member.username} is ${ranks[data.rank]}`
+            },
+            "description": [
+                `**${this.get("history_with_winloss")}:** ${data.winlose.win}/${data.winlose.lose}`,
+                `**${this.get("history_as_games")}:** ${data.winlose.win + data.winlose.lose}`,
+                `**${this.get("history_as_winrate")}:** ${Math.round(data.winlose.win / (data.winlose.win + data.winlose.lose) * 10000) / 100}%`
+            ].join("\n"),
+            "timestamp": new Date(Date.now()),
+            "footer": {
+                "text": this.get("prommr_last_updated")
+            },
+            "thumbnail": {
+                "url": "attachment://rank.png"
+            }
         }
     };
 }

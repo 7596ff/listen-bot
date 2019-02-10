9a40c5a40edb7d3cb7771fde6d565174c592f1c9
Cassandra McCarthy <cassie@7596ff.com>
2019-02-09 19:24:02 -0500

    add new rank names

--- embeds/singleMmr.js
+++ embeds/singleMmr.js
@@ -10,6 +10,8 @@ const ranks = {
     "13": "Herald [3]",
     "14": "Herald [4]",
     "15": "Herald [5]",
+    "16": "Herald [6]",
+    "17": "Herald [7]",
     
     "20": "Guardian",
     "21": "Guardian [1]",
@@ -17,6 +19,8 @@ const ranks = {
     "23": "Guardian [3]",
     "24": "Guardian [4]",
     "25": "Guardian [5]",
+    "26": "Guardian [6]",
+    "27": "Guardian [7]",
     
     "30": "Crusader",
     "31": "Crusader [1]",
@@ -24,6 +28,8 @@ const ranks = {
     "33": "Crusader [3]",
     "34": "Crusader [4]",
     "35": "Crusader [5]",
+    "36": "Crusader [6]",
+    "37": "Crusader [7]",
     
     "40": "Archon",
     "41": "Archon [1]",
@@ -31,6 +37,8 @@ const ranks = {
     "43": "Archon [3]",
     "44": "Archon [4]",
     "45": "Archon [5]",
+    "46": "Archon [6]",
+    "47": "Archon [7]",
     
     "50": "Legend",
     "51": "Legend [1]",
@@ -38,6 +46,8 @@ const ranks = {
     "53": "Legend [3]",
     "54": "Legend [4]",
     "55": "Legend [5]",
+    "56": "Legend [6]",
+    "57": "Legend [7]",
     
     "60": "Ancient",
     "61": "Ancient [1]",
@@ -45,6 +55,8 @@ const ranks = {
     "63": "Ancient [3]",
     "64": "Ancient [4]",
     "65": "Ancient [5]",
+    "66": "Ancient [6]",
+    "67": "Ancient [7]",
     
     "70": "Divine",
     "71": "Divine [1]",
@@ -52,6 +64,8 @@ const ranks = {
     "73": "Divine [3]",
     "74": "Divine [4]",
     "75": "Divine [5]",
+    "76": "Divine [6]",
+    "77": "Divine [7]",
 
     "80": "Immortal",
     "81": "Immortal",
